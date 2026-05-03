ca83d1832e75e254c9c4b2a9d6cb6cfaec110c50
bridgem2017 <bridgem2017@gmail.com>
2026-05-03 15:25:04 +0900

    foil v46 popup update

--- popup-config.js
+++ popup-config.js
@@ -5,19 +5,19 @@
 // - image: 배너 이미지 주소 또는 파일 경로
 // - 여러 개 추가 가능
 window.FOIL_POPUPS = [
-  {
-  "id": "popup-1777782533042",
+ {
+  "id": "popup-1777789431096",
   "enabled": true,
   "type": "modal",
   "badge": "가맹영업 사례",
   "brand": "1KM 휴게소 분식",
-  "headline": "영업 10개월 만에!!",
-  "text": "온라인 리드 수집와 오프라인 영업을 병행하여 10개월만에 가맹점 확대",
+  "headline": "100호점 돌파!!",
+  "text": "온라인 리드 수집과 오프라인 영업 병행으로 가맹점 확대!",
   "image": "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?auto=format&fit=crop&w=900&q=85",
   "button": "문의하기",
   "link": "#contact",
   "position": "center",
   "theme": "green",
-  "onceKey": "foil_popup_1777782533042"
+  "onceKey": "foil_popup_1777789431096"
 },
 ];
